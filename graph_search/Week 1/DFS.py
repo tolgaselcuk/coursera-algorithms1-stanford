46ba6c4c07e311a3070e85a04e611cb8bb5ca9eb
tolgaselcuk <tolga.selcuk@gmail.com>
2017-02-03 08:08:30 +0300

    topological order with recursive DFS

--- graph_search/Week 1/DFS.py
+++ graph_search/Week 1/DFS.py
@@ -15,6 +15,7 @@ def load_graph_into_dict(f):
         d[vertice] = edges
     return d
 
+
 def DFS_recursive(g,s,visited=None, route=None):
     nodes = g.keys()
     if visited == None:
@@ -33,7 +34,6 @@ def DFS_recursive(g,s,visited=None, route=None):
         if not visited[k[1]]:
             visited, route = DFS_recursive(g,k[1],visited,route)
     return visited, route
-
 
 def DFS(g,s):
     nodes = g.keys()
@@ -68,15 +68,61 @@ def DFS(g,s):
 
     return visited, route
 
+def DFS_topological_order(g, s, f, current_label, visited=None, route=None):
 
-d = load_graph_into_dict("./smallGraph2.txt")
-start_vertex = d.keys()[0]
-connected, route = DFS(d, start_vertex)
-print "connected", connected, "\n"
-print "route", route, "\n---------\n"
+    if visited == None:
+        visited = dict(zip(nodes, [0 for i in nodes]))
+    if route == None:
+        route = []
 
-d = load_graph_into_dict("./smallGraph2Labels.txt")
-start_vertex = d.keys()[0]
-connected_rec, route_rec = DFS_recursive(d, start_vertex)
-print "connected", connected_rec, "\n"
-print "recursive route", route_rec, "\n"
+    visited[s] += 1
+    if DEBUG_MODE: print "visited : %s" % visited
+
+    route.append(s)
+    if DEBUG_MODE: print "route : %s" % route
+
+    for k in g[s]:
+        if DEBUG_MODE: print "k : %s" % (k,)
+        if not visited[k[1]]:
+            visited, route, f, current_label = DFS_topological_order(g, k[1], f, current_label, visited,route)
+    f[s] = current_label
+    current_label = current_label - 1
+    return visited, route, f, current_label
+
+def DFS_Loop(g, current_label = None):
+    #init nodes and mark all unexplored
+    nodes = g.keys()
+    explored = dict(zip(nodes,[0 for i in nodes]))
+    f = dict(zip(nodes,[0 for i in nodes]))
+
+    #init the current label as max
+    if current_label == None:
+        current_label = len(nodes)
+
+    #traverse all vertices
+    for v in nodes:
+        if not explored[v]:
+            explored, route, f, current_label = DFS_topological_order(g,v,f,current_label,explored)
+
+    return f, current_label
+
+# d = load_graph_into_dict("./smallGraph2.txt")
+# start_vertex = d.keys()[0]
+# connected, route = DFS(d, start_vertex)
+# print "connected", connected, "\n"
+# print "route", route, "\n---------\n"
+#
+# d = load_graph_into_dict("./smallGraph2Labels.txt")
+# start_vertex = d.keys()[0]
+# connected_rec, route_rec = DFS_recursive(d, start_vertex)
+# print "connected", connected_rec, "\n"
+# print "recursive route", route_rec, "\n---------\n"
+#
+# d = load_graph_into_dict("./directedWithLabels.txt")
+# start_vertex = d.keys()[0]
+# connected_rec, route_rec = DFS_recursive(d, start_vertex)
+# print "connected", connected_rec, "\n"
+# print "recursive route", route_rec, "\n---------\n"
+
+d = load_graph_into_dict("./directedWithLabels.txt")
+print DFS_Loop(d)
