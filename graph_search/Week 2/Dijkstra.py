e9321b700e022a3e42128e10b350c43f7c4537eb
tolgaselcuk <tolga.selcuk@gmail.com>
2017-02-11 23:16:28 +0300

    loaded dijkstra data text

---
+++ graph_search/Week 2/Dijkstra.py
@@ -0,0 +1,20 @@
+
+def load_graph_into_dict(f):
+    data = [line.strip().split("\t") for line in open(f, 'r')]
+
+    d = {}
+    for k in data:
+        vertice = head = k[0]
+        tails = k[1:]
+        tails2 = [t.split(',') for t in tails]
+        len_tails = len(tails)
+        heads = [head for i in range(len_tails)]
+        edges = zip(heads,[t[0] for t in tails2],[t[1] for t in tails2])
+        d[vertice] = edges
+    return d
+
+
+
+d = load_graph_into_dict("./dijkstraData.txt")
+print "\n\n"
+print d
