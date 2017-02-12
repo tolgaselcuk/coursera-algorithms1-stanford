c3c3557c07a380498e5eddefcfa6dde1b4651bf7
tolgaselcuk <tolga.selcuk@gmail.com>
2017-02-12 17:13:24 +0300

    computed for dijkstraData in the programming assingment

--- graph_search/Week 2/Dijkstra.py
+++ graph_search/Week 2/Dijkstra.py
@@ -1,3 +1,4 @@
+DEBUG_MODE = False
 
 def load_graph_into_dict(f):
     data = [line.strip().split("\t") for line in open(f, 'r')]
@@ -17,51 +18,58 @@ def getKey(item):
     return item[2]
 
 def choose_shortest_edge(X, e):
-    print "-------\nchoose shortest edge\ne:%s" %e
+    if DEBUG_MODE: print "-------\nchoose shortest edge\ne:%s" %e
     new_e = []
     for i in e:
-        print "i:%s,%s,%s" % i
+        if DEBUG_MODE: print "i:%s,%s,%s" % i
         value = i[2] + X[i[0]][0]
         new_e.append((i[0],i[1],value))
     new_e.sort(key=getKey)
-    print "chosen:%s\n-------" % (new_e[0],)
+    if DEBUG_MODE: print "chosen:%s\n-------" % (new_e[0],)
     return new_e[0]
 
 def shorttest_path(g, s):
     X = {}
 
     X[s] = [0,'']
-    print "X:%s" % X
-    print "len_X:%s, len_g:%s" % (len(X), len(g))
+    if DEBUG_MODE: print "X:%s" % X
+    if DEBUG_MODE: print "len_X:%s, len_g:%s" % (len(X), len(g))
 
     while len(X) <> len(g):
-        print "====Run===="
+        if DEBUG_MODE: print "====Run===="
         edges = [g[k] for k in X]
         edges2 = [x for t in edges for x in t]
-        print "edges2:%s" % edges2
+        if DEBUG_MODE: print "edges2:%s" % edges2
         remove_list = []
         for e in edges2:
-            print "e:%s" % (e,)
+            if DEBUG_MODE: print "e:%s" % (e,)
             if X.has_key(e[1]):
                 remove_list.append(e)
-        print "remove list:%s" % remove_list
+        if DEBUG_MODE: print "remove list:%s" % remove_list
         if len(remove_list):
             edges2 = [x for x in edges2 if x not in remove_list]
-        print "edges2:%s" % edges2
+        if DEBUG_MODE: print "edges2:%s" % edges2
         shortest = choose_shortest_edge(X,edges2)
-        print "shortest:%s" % (shortest,)
+        if DEBUG_MODE: print "shortest:%s" % (shortest,)
         X[shortest[1]] = [shortest[2], X[shortest[0]][1] +shortest[1] + ', ']
-        print "X:%s" % X
+        if DEBUG_MODE: print "X:%s" % X
 
     return X
 
 
-#d = load_graph_into_dict("./dijkstraData.txt")
-d = load_graph_into_dict("./testdata2.txt")
-print d, len(d)
+d = load_graph_into_dict("./dijkstraData.txt")
+#d = load_graph_into_dict("./testdata2.txt")
+#print d, len(d)
 print "\n\n"
 
-shorttest_path(d,'1')
+paths = shorttest_path(d,'1')
+print paths
+
+for i in (7,37,59,82,99,115,133,165,188,197):
+    if paths.has_key(str(i)):
+        print "%s," % paths[str(i)][0]
+    else:
+        print '1000000'
 
 #q=[('1', '2', 1), ('1', '8', 2), ('2', '1', 1), ('2', '3', 1)]
 #r = [('1', '2', 1), ('2', '1', 1)]
